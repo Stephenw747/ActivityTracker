06c170706bf5cb8d96473d3444c1e08525b98e60
Stephen <stephenw747@gmail.com>
2020-04-12 16:23:16 -0400

    f

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,61 @@
 import java.util.Scanner;
 
 public class Main {
-
+	
 	public static void main(String[] args) {		
-		userName();
-		milesRan();		
-		converter();		
+		userName();	
+		converter(totalMilesRan(dailyMilesRan()));
+		
 	}
 	
-	public static void userName() {
+	public static String userName() {
 		System.out.println("What is your username?");
 		
 		Scanner username = new Scanner(System.in);
 		String userName = username.nextLine();
 		
-		System.out.println("Congrats " + userName);
+		System.out.println("Hello " + userName);
+		return userName;
 	}
 	
 	
 	
-	public static double milesRan() {
-		double totalMiles = 0;
-		
+	public static double dailyMilesRan() {
+
 		System.out.println("How many miles did you run?");
 		
 		Scanner milesran = new Scanner(System.in);
-		double milesRan = milesran.nextDouble();
-		totalMiles += milesRan;	
+		double dailyMilesRan = milesran.nextDouble();	
 		
-		System.out.println("Your new total miles is: " + totalMiles);
-		return totalMiles;
+		System.out.println("Congratulations on running " + dailyMilesRan + " miles!!");
+		return dailyMilesRan;
 	}
 	
 	
+	public static double totalMilesRan(double dailyMilesRan) {
+		double totalMilesRan = 0;
+		totalMilesRan += dailyMilesRan;
+		return totalMilesRan;
+	}
 	
-	public static double converter(double totalMiles) {
-		double miles = totalMiles;
-		double kilometers = miles * 1.60934;
+	
+	public static double converter(double totalMilesRan) {
+		
+		Scanner kilos = new Scanner(System.in);
+		
+		double kilometers = totalMilesRan * 1.60934;
 		
 		System.out.println("Would you like to convert your total to kilometers?");		
-		
-		
-		Scanner kilos = new Scanner(System.in);		
+			
 		String answer = kilos.nextLine();
 		
-		if (answer == "Yes") {
-			System.out.println("Your Kilometers ran is: " + kilometers);
-		} else {
-			System.out.println("Alright what would you like to do next?");
-		}
-		return miles;
-	}
-
-	
-	
-	
+			if (answer == "Yes") {
+				System.out.println("Your Kilometers ran is: " + kilometers);
+			} else {
+				System.out.println("Why is this the only answer it defaults to?");
+			}
+		
+		
+		return totalMilesRan;
+	}	
 }
